769228b299ae8cd3764d77f38ba845b9db4797d9
mpodles <michal.piotr.podles@gmail.com>
2024-01-26 13:20:56 +0000

    Stats gathering working

--- pmbw.cc
+++ pmbw.cc
@@ -18,7 +18,7 @@
  *
  * This program is distributed in the hope that it will be useful, but WITHOUT
  * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or
- * FITNESS FOR A PARTICULAR PURPOSE.  Seethe GNU General Public License for
+ * FITNESS FOR A PARTICULAR PURPOSE.  See the GNU General Public License for
  * more details.
  *
  * You should have received a copy of the GNU General Public License along with
@@ -45,294 +45,8 @@
 
 #include <stdint.h>
 #include <stdio.h>
-#include <utilities.h>
 
-enum status {
-  SUCCESS,
-  ERROR_NOT_FOUND,
-  IO_FAILED,
-  INVALID_USE,
-  NO_MEMORY,
-  BAD_STATE
-};
-
-struct dpu_statistics
-{
-  uint64_t memory_read;
-  uint64_t memory_write;
-  uint64_t reads_difference;
-  uint64_t writes_difference;
-};
-
-// This structure is returned each time the main measurement is read.
-// It contains the result of all the measurements, taken simultaneously.
-// The order of the measurements is unknown - used the ID.
-typedef struct {
-  uint64_t recorded_values;
-  struct {
-    uint64_t value;
-    uint64_t id;
-  } values[6];
-} measurement_t;
-
-// The main measuring group.
-perf_measurement_t *all_measurements;
-// Retired instructions. Be careful, these can be affected by various issues, most notably hardware interrupt counts.
-perf_measurement_t *measure_instruction_count;
-// Total cycles; not affected by CPU frequency scaling.
-perf_measurement_t *measure_cycle_count;
-// This counts context switches. Until Linux 2.6.34, these were all reported as user-space events, after that they are reported as happening in the kernel.
-perf_measurement_t *measure_context_switches;
-// This reports the CPU clock, a high-resolution per-CPU timer.
-// See also: https://stackoverflow.com/questions/23965363/linux-perf-events-cpu-clock-and-task-clock-what-is-the-difference.
-perf_measurement_t *measure_cpu_clock;
-// This counts the number of branch misses branch misses. Retired branch instructions.  Prior to Linux 2.6.35, this used the wrong event on AMD processors
-perf_measurement_t *measure_cpu_branches;
-
-perf_measurement_t *measure_l1_cache_reads;
-
-static int prepared_successfully = 0;
-
-// Call prepare before executing main
-void prepare() __attribute__((constructor));
-// Call cleanup before exiting
-void cleanup() __attribute__((destructor));
-
-void assert_support() {
-  // Print the kernel version
-  int major, minor, patch;
-  int status = perf_get_kernel_version(&major, &minor, &patch);
-  if (status < 0) {
-    perf_print_error(status);
-    exit(EXIT_FAILURE);
-  }
-
-  fprintf(stderr, "Kernel version: %d.%d.%d\n", major, minor, patch);
-
-  // Exit if the API is unsupported
-  status = perf_is_supported();
-  if (status < 0) {
-    perf_print_error(status);
-    exit(EXIT_FAILURE);
-  } else if (status == 0) {
-    fprintf(stderr, "error: perf not supported\n");
-    exit(EXIT_FAILURE);
-  }
-}
-
-void prepare_measurement(const char *description, perf_measurement_t *measurement, perf_measurement_t *parent_measurement) {
-  int status = perf_has_sufficient_privilege(measurement);
-  if (status < 0) {
-    perf_print_error(status);
-    exit(EXIT_FAILURE);
-  } else if (status == 0) {
-    fprintf(stderr, "error: unprivileged user\n");
-    exit(EXIT_FAILURE);
-  }
-
-  int support_status = perf_event_is_supported(measurement);
-  if (support_status < 0) {
-    perf_print_error(support_status);
-    exit(EXIT_FAILURE);
-  } else if (support_status == 0) {
-    fprintf(stderr, "warning: %s not supported\n", description);
-    return;
-  }
-
-  int group = parent_measurement == NULL ? -1 : parent_measurement->file_descriptor;
-
-  status = perf_open_measurement(measurement, group, 0);
-  if (status < 0) {
-    perf_print_error(status);
-    exit(EXIT_FAILURE);
-  }
-}
-
-void prepare() {
-  fprintf(stderr, "preparing harness\n");
-
-  // Fail if the perf API is unsupported
-  assert_support();
-
-  // Create a dummy measurement (measures nothing) to act as a group leader
-  all_measurements = perf_create_measurement(PERF_TYPE_SOFTWARE, PERF_COUNT_SW_DUMMY, 0, -1);
-  prepare_measurement("software dummy counter", all_measurements, NULL);
-
-  // Measure the number of retired instructions
-  measure_instruction_count = perf_create_measurement(PERF_TYPE_HARDWARE, PERF_COUNT_HW_INSTRUCTIONS, 0, -1);
-  measure_instruction_count->attribute.exclude_kernel = 1;
-  prepare_measurement("hardware instruction counter", measure_instruction_count, all_measurements);
-
-  // Measure the number of CPU cycles (at least on Intel CPUs, see https://perf.wiki.kernel.org/index.php/Tutorial#Default_event:_cycle_counting)
-  measure_cycle_count = perf_create_measurement(PERF_TYPE_HARDWARE, PERF_COUNT_HW_REF_CPU_CYCLES, 0, -1);
-  measure_cycle_count->attribute.exclude_kernel = 1;
-  prepare_measurement("hardware cycles counter", measure_cycle_count, all_measurements);
-
-  // Measure the number of context switches
-  measure_context_switches = perf_create_measurement(PERF_TYPE_SOFTWARE, PERF_COUNT_SW_CONTEXT_SWITCHES, 0, -1);
-  prepare_measurement("software context switches counter", measure_context_switches, all_measurements);
-
-  // Measure the CPU clock related to the task (see https://stackoverflow.com/questions/23965363/linux-perf-events-cpu-clock-and-task-clock-what-is-the-difference)
-  measure_cpu_clock = perf_create_measurement(PERF_TYPE_SOFTWARE, PERF_COUNT_SW_TASK_CLOCK, 0, -1);
-  prepare_measurement("software task clock", measure_cpu_clock, all_measurements);
-
-  // Measure CPU branches
-  measure_cpu_branches = perf_create_measurement(PERF_TYPE_HARDWARE, PERF_COUNT_HW_BRANCH_MISSES, 0, -1);
-  prepare_measurement("cpu branches counter", measure_cpu_branches, all_measurements);
-  // Create a measurement using hardware (CPU) registers. Measure the number of instructions amassed.
-  measure_l1_cache_reads = perf_create_measurement(PERF_TYPE_HW_CACHE, 
-                                            (PERF_COUNT_HW_CACHE_L1D | PERF_COUNT_HW_CACHE_OP_READ<<8 | PERF_COUNT_HW_CACHE_RESULT_ACCESS << 16),
-                                            0,
-                                            -1);
-  prepare_measurement("l1_cache_reads", measure_l1_cache_reads, all_measurements);
-  // Mark the preparation stage as successfuly
-  prepared_successfully = 1;
-}
-
-void cleanup() {
-  fprintf(stderr, "cleaning up harness\n");
-  if (all_measurements != NULL) {
-    perf_close_measurement(all_measurements);
-    free((void *)all_measurements);
-  }
-
-  if (measure_instruction_count != NULL) {
-    perf_close_measurement(measure_instruction_count);
-    free((void *)measure_instruction_count);
-  }
-
-  if (measure_cycle_count != NULL) {
-    perf_close_measurement(measure_cycle_count);
-    free((void *)measure_cycle_count);
-  }
-
-  if (measure_context_switches != NULL) {
-    perf_close_measurement(measure_context_switches);
-    free((void *)measure_context_switches);
-  }
-
-  if (measure_cpu_clock != NULL) {
-    perf_close_measurement(measure_cpu_clock);
-    free((void *)measure_cpu_clock);
-  }
-}
-
-void print_perf_measurements(std::ostringstream &result, measurement_t *measurement) {
-    uint64_t values[7] = {0};
-    perf_measurement_t *taken_measurements[] = {all_measurements, measure_instruction_count, measure_cycle_count, measure_context_switches, measure_cpu_clock, measure_cpu_branches, measure_l1_cache_reads};
-
-    for (uint64_t j = 0; j < measurement->recorded_values; j++) {
-      for (int k = 0; k < 7; k++) {
-        if (measurement->values[j].id == taken_measurements[k]->id) {
-          values[k] = measurement->values[j].value;
-          break;
-        }
-      }
-    }
-    // Ignore the results from the dummy counter
-    result<<" instructions="<<values[1]<< '\t'<<
-            " cycles=" <<values[2]<< '\t'<<
-            " context_switches=" <<values[3]<< '\t'<<
-            " clock=" <<values[4]<< '\t'<<
-            " cpu_branches=" <<values[5]<< '\t'<<
-            " l1_read_hit=" <<values[6];
-}
-
-static struct dpu_statistics g_dpu_stats = {.memory_read = 0, .reads_difference=0};
-
-int
-read_file(char const *path, char **out_bytes, size_t *out_bytes_len)
-{
-	FILE *file;
-	char *bytes;
-
-	file = fopen(path, "rb");
-	if (file == NULL)
-		return ERROR_NOT_FOUND;
-
-	if (fseek(file, 0, SEEK_END) != 0) {
-		fclose(file);
-		return IO_FAILED;
-	}
-
-	long const nb_file_bytes = ftell(file);
-
-	if (nb_file_bytes == -1) {
-		fclose(file);
-		return IO_FAILED;
-	}
-
-	if (nb_file_bytes == 0) {
-		fclose(file);
-		return INVALID_USE;
-	}
-
-	bytes = (char *)malloc(nb_file_bytes);
-	if (bytes == NULL) {
-		fclose(file);
-		return NO_MEMORY;
-	}
-
-	if (fseek(file, 0, SEEK_SET) != 0) {
-		free(bytes);
-		fclose(file);
-		return IO_FAILED;
-	}
-
-	size_t const read_byte_count = fread(bytes, 1, nb_file_bytes, file);
-
-	fclose(file);
-
-	// if (read_byte_count != (size_t)nb_file_bytes) {
-	// 	free(bytes);
-	// 	return DOCA_ERROR_IO_FAILED;
-	// }
-
-	*out_bytes = bytes;
-	*out_bytes_len = read_byte_count;
-	return 0;
-}
-
-int
-open_file(char *fname, FILE **file)
-{
-    // char fname[100];
-    // sprintf(fname, "/tmp/build/stats%d", test_num);
-    *file = fopen(fname, "r");
-
-    if(*file == NULL)
-    {
-      return BAD_STATE;
-    }
-  return SUCCESS;
-}
-
-
-int
-read_hw_counters(std::string fname, uint64_t *counter)
-{
-  char *file_contents;
-  uint64_t file_len;
-  int result = read_file(fname.c_str(), &file_contents, &file_len);
-  // Last byte is /n, replace it with string end for correct parsing
-  file_contents[file_len] = 0;
-  *counter = strtouq(file_contents, NULL, 0);
-  free(file_contents);
-
-  return result;
-}
-
-void
-update_dpu_counters(uint8_t tile)
-{
-  uint64_t memory_read;
-  uint64_t memory_write;
-  
-  read_hw_counters("/sys/class/hwmon/hwmon0/tile"+std::to_string(tile) +"/counter0", &memory_read);
-  read_hw_counters("/sys/class/hwmon/hwmon0/tile"+std::to_string(tile) +"/counter1", &memory_write);
-  g_dpu_stats.reads_difference = memory_read - g_dpu_stats.memory_read;
-  g_dpu_stats.memory_read = memory_read;
-}
+#include "stats.h"
 
 #if ON_WINDOWS
 #include <windows.h>
@@ -811,6 +525,12 @@ void* thread_master(void* cookie)
 
     pin_self_to_core(thread_num);
 
+    prepare();
+    // measure_l1_cache_reads = perf_create_measurement(PERF_TYPE_HW_CACHE, 
+    //                                           (PERF_COUNT_HW_CACHE_L1D | PERF_COUNT_HW_CACHE_OP_READ<<8 | PERF_COUNT_HW_CACHE_RESULT_ACCESS << 16),
+    //                                           0,
+    //                                           -1);
+    // prepare_measurement("l1_cache_reads", measure_l1_cache_reads, NULL);
     // // Ensure that the caller has sufficient privilege for performing the measurement
     // int has_sufficient_privilege = perf_has_sufficient_privilege(measure_l1_cache_reads);
     // if (has_sufficient_privilege != 1) {
@@ -912,7 +632,8 @@ void* thread_master(void* cookie)
                 update_dpu_counters(thread_num/2);
 
                 // Reset the counter and start measuring
-                perf_start_measurement(all_measurements);
+                perf_start_measurement(measure_l1_read_hit);
+                // perf_start_measurement(measure_l1_cache_reads);
 
                 // *** Barrier ****
                 pthread_barrier_wait(&g_barrier);
@@ -926,8 +647,8 @@ void* thread_master(void* cookie)
                 double ts2 = timestamp();
                 update_dpu_counters(thread_num/2);
 
-                perf_stop_measurement(all_measurements);
-                perf_read_measurement(all_measurements, &measurement, sizeof(measurement_t));
+                perf_stop_measurement(measure_l1_read_hit);
+                perf_read_measurement(measure_l1_read_hit, &measurement, sizeof(measurement_t));
 
                 runtime = ts2 - ts1;
             }
@@ -988,10 +709,7 @@ void* thread_master(void* cookie)
 
     // *** Barrier ****
     pthread_barrier_wait(&g_barrier);
-    perf_close_measurement(measure_l1_cache_reads);
-
-    // Always free any allocated measurement
-    free((void *)measure_l1_cache_reads);
+    cleanup_perf();
     return NULL;
 }
 
